098d134a00c43f88d9dfd0bc8cfcd33b4e31a0e5
Roman Salo <frs.daddy@gmail.com>
2021-02-23 14:27:04 +0200

    YANG-4702: Add the group visibility field to courses

--- modules/social_course_advanced/src/SocialCourseAdvancedOverrides.php
+++ modules/social_course_advanced/src/SocialCourseAdvancedOverrides.php
@@ -37,6 +37,37 @@ class SocialCourseAdvancedOverrides implements ConfigFactoryOverrideInterface {
       ];
     }
 
+    $config_name = 'core.entity_form_display.group.course_advanced.default';
+
+    if (in_array($config_name, $names)) {
+      $config = \Drupal::service('config.factory')->getEditable($config_name);
+
+      $children = $config->get('third_party_settings.field_group.group_settings.children');
+      $children[] = 'field_flexible_group_visibility';
+
+      $content = $config->get('content');
+      $content['field_flexible_group_visibility'] = [
+        'weight' => 100,
+        'settings' => [
+          'display_label' => TRUE,
+        ],
+        'third_party_settings' => [],
+        'type' => 'options_buttons',
+        'region' => 'content',
+      ];
+
+      $overrides[$config_name] = [
+        'third_party_settings' => [
+          'field_group' => [
+            'group_settings' => [
+              'children' => $children,
+            ],
+          ],
+        ],
+        'content' => $content,
+      ];
+    }
+
     return $overrides;
   }
 
